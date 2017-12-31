20e2464e940f1192414547668fab642a8ad9271a
Sunny Kotwal <sunnykotwal@yahoo.com>
2017-12-31 01:56:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipe = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({},object, {[key]: value })
+  return Object.assign({},object, {[key]: value }) ;
 }
